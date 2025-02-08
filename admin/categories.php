209fa927db9eae09195ea702b946066decaf42bd
iman <imanelsebeiy@gmail.com>
2025-02-08 14:12:27 +0200

    product and category

---
+++ admin/categories.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once '../includes/conn.php';
+require_once "../includes/utils.php";
+
+try {
+    $pdo = connectToDB();
+    $sql="SELECT * FROM `categories`";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    $categories = $stmt->fetchAll();
+    // var_dump($categories);
+
+}catch (PDOException $e){
+    displayError($e->getMessage());
+    return false;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin Dashboard</title>
+  </head>
+  <body>
+    <main>
+    <?php
+    //  require_once "../includes/nav.php";
+     ?>
+      <div class="container my-5">
+        <div class="bg-light p-5 rounded">
+          <h2 class="fw-bold fs-2 mb-5 pb-2">All categories</h2>
+          <div class="table-responsive">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Category</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+            <?php
+              foreach ($categories as $category) {
+              ?>
+              <tr>
+                <th scope="row"><?php echo $category['category_id']?></th>
+                 <td><?php echo $category['name'] ?></td>
+                <td><a href="edit_category.php?id=<?php echo $category['category_id'] ?>" class="text-decoration-none"><i><img src="../imgs/edit.png" style="max-width: 35px" alt="edit.png"></i></a></td>
+                <td><a href="deletetech.php?id=<?php echo $category['category_id'] ?>" onclick="return confirm('Are you sure you want to delete?')" class="text-decoration-none"><img src="../imgs/delete.png" alt="" style="max-width: 35px"></a></td>
+              </tr>
+              <?php
+                 }
+                ?>
+            </tbody>
+          </table>
+        </div>
+        </div>
+      </div>
+    </main>
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
---
+++ admin/products.php
@@ -0,0 +1,55 @@
+<?php
+require_once "../includes/conn.php";
+require_once "../includes/utils.php";
+
+try {
+    $pdo = connectToDB();
+    $select_query ="SELECT * FROM `categories`, `products` WHERE products.`category_id` = `categories`.`category_id`;";
+
+    $stmt = $pdo->prepare($select_query);
+    $stmt->execute();
+
+    $products = $stmt->fetchAll();
+    var_dump($products);
+
+}catch (PDOException $e){
+    displayError($e->getMessage());
+    return false;
+}
+?>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Image</th>
+                <th>Quantity</th>
+                <th>Availability</th>
+                <th>Category</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+            <?php
+              foreach ($products as $product) {
+              ?>
+              <tr>
+                 <td><?php echo $product['product_name'] ?></td>
+                 <th><?php echo $product['price']?> EL</th>
+                 <td> <img src="<?php echo $product['image'] ?>" width="100px" height="100px" alt=""></td>
+                 <th><?php echo $product['quantity']?></th>
+                 <td><?php if($product['quantity']>0){
+                            echo 'Available';
+                  	      } else {
+                           echo 'Unvailable';
+                     	}?></td>
+                 <td><?php echo $product['name'] ?></td>
+                <td><a href="edit_product.php?id=<?php echo $product['product_id'] ?>" class="text-decoration-none"><i><img src="../imgs/edit.png" style="max-width: 30px" alt=""></i></a></td>
+                <td><a href="deletetech.php?id=<?php echo $product['product_id'] ?>" onclick="return confirm('Are you sure you want to delete <?php echo $product['product_name'] ?>?')"class="text-decoration-none"><img src="../imgs/delete.png" alt="" style="max-width: 30px"></a></td>
+              </tr>
+              <?php
+                 }
+                ?>
+            </tbody>
+          </table>
